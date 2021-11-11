4dfa8dac4ee28ac833de1c9b03fcf857c9940785
ayuj6 <ayujprasad1999@gmail.com>
2021-11-11 02:59:04 -0600

    Created sign-up view. Buttons and footer are now functional to switch between views when clicked.

--- app/src/main/java/com/example/wavelength/SignupActivity.java
+++ app/src/main/java/com/example/wavelength/SignupActivity.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); //Test
+        setContentView(R.layout.signup);
     }
 
-    public void toSignUp(View view){
-        Intent intent  = new Intent(this, SignupActivity.class);
+    public void toSignIn(View view){
+        Intent intent  = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
