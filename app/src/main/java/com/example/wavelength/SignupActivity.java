58e950c203eac00fcea1fe0c81ff0ed91c1157a3
Meerxn <fardeenfmeeran@gmail.com>
2021-11-29 12:21:16 -0600

    Added Firebase auth

--- app/src/main/java/com/example/wavelength/SignupActivity.java
+++ app/src/main/java/com/example/wavelength/SignupActivity.java
@@ -1,16 +1,27 @@
 package com.example.wavelength;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignupActivity extends AppCompatActivity {
+    private static final String TAG = "EmailPassword";
 
+    private FirebaseAuth mAuth;
     EditText usernamebox;
     EditText passwordbox;
     @Override
@@ -20,6 +31,8 @@ public class SignupActivity extends AppCompatActivity {
 
         usernamebox = (EditText) findViewById(R.id.username_su);
         passwordbox = (EditText) findViewById(R.id.password_su);
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
     public void toSignIn(View view){
@@ -27,12 +40,32 @@ public class SignupActivity extends AppCompatActivity {
         Context context = getApplicationContext();
         SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("users", Context.MODE_PRIVATE, null);
         DBHelper dbHelper = new DBHelper(sqLiteDatabase);
+        String usr = usernamebox.getText().toString();
+        String password = passwordbox.getText().toString();
+        String email = usernamebox.getText().toString();
 
-       dbHelper.onSignup(usernamebox.getText().toString(), passwordbox.getText().toString());
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+
+                        }
+                    }
+                });
+        dbHelper.onSignup(usr, password);
+
 
         Intent intent  = new Intent(this, MainActivity.class);
-        intent.putExtra("username_su", usernamebox.getText().toString());
-        intent.putExtra("password_su", passwordbox.getText().toString());
+        intent.putExtra("username_su", usr);
+        intent.putExtra("password_su", password);
         startActivity(intent);
     }
 }
