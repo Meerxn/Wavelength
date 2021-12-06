5c73e7803bb7730cab9b7d7a7dff6a5fe59bd763
ayuj6 <ayujprasad1999@gmail.com>
2021-12-06 15:57:23 -0600

    Added confirmation page and user profile page

---
+++ app/src/main/java/com/example/wavelength/Confirmation_Page.java
@@ -0,0 +1,14 @@
+package com.example.wavelength;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Confirmation_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmation_page);
+    }
+}
