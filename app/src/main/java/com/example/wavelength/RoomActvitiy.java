1c487d33cd15dc064a79ba42f825c56542dd40da
harininarayan <harini.s.narayan@gmail.com>
2021-12-06 14:36:45 -0600

    adding image and room title to room page

--- app/src/main/java/com/example/wavelength/RoomActvitiy.java
+++ app/src/main/java/com/example/wavelength/RoomActvitiy.java
@@ -2,15 +2,28 @@ package com.example.wavelength;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class RoomActvitiy extends AppCompatActivity {
     TextView libName;
+    TextView roomName;
+    TextView date;
+    private DatePickerDialog datePickerDialog;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +37,52 @@ public class RoomActvitiy extends AppCompatActivity {
         View view = inflater.inflate(R.layout.custom_action_bar, null);
         actionBar.setCustomView(view);
 
+        //setting lib and room name
         libName = (TextView) findViewById(R.id.libName);
+        roomName = (TextView) findViewById(R.id.roomName);
         Intent intent = getIntent();
         String libNameStr = intent.getStringExtra("libName").split(", ")[0];
         String roomNameStr = intent.getStringExtra("libName").split(", ")[1];
         libName.setText(libNameStr);
+        roomName.setText("Room: " + roomNameStr);
+
+        date = (TextView) findViewById(R.id.date_text);
+        date.setText(todayDate());
+
+    }
+
+    public void showDatePickerDialog(View view){
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                month = month + 1;
+                String dayStr = month + "/" + day + "/" + year;
+                date.setText(dayStr);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_DARK;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+        datePickerDialog.show();
+
+    }
+
+
+    private String todayDate()
+    {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return month + "/" + day + "/" + year;
     }
 }
