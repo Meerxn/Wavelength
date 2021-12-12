6ba45683e70a09e32b5bc2b12e2884d837ada54b
nikhsub <venkatasubr2@wisc.edu>
2021-12-12 15:24:51 -0600

    Added Reservation Fragment with Recycler view

---
+++ app/src/main/java/com/example/wavelength/MyItemRecyclerViewAdapter.java
@@ -0,0 +1,64 @@
+package com.example.wavelength;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.wavelength.placeholder.PlaceholderContent.PlaceholderItem;
+import com.example.wavelength.databinding.FragmentReservationBinding;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link PlaceholderItem}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyItemRecyclerViewAdapter extends RecyclerView.Adapter<MyItemRecyclerViewAdapter.ViewHolder> {
+
+    private final List<PlaceholderItem> mValues;
+
+    public MyItemRecyclerViewAdapter(List<PlaceholderItem> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        return new ViewHolder(FragmentReservationBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).roomid);
+        holder.mContentView.setText(mValues.get(position).roomid+" "+mValues.get(position).starttime+" "+mValues.get(position).endtime+
+                " "+mValues.get(position).name+" "+mValues.get(position).date);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mIdView;
+        public final TextView mContentView;
+        //public final TextView mStartTimeView;
+        public PlaceholderItem mItem;
+
+        public ViewHolder(FragmentReservationBinding binding) {
+            super(binding.getRoot());
+            mIdView = binding.itemNumber;
+            mContentView = binding.content;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
---
+++ app/src/main/java/com/example/wavelength/placeholder/PlaceholderContent.java
@@ -0,0 +1,73 @@
+package com.example.wavelength.placeholder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class PlaceholderContent {
+
+    /**
+     * An array of sample (placeholder) items.
+     */
+    public static final List<PlaceholderItem> ITEMS = new ArrayList<PlaceholderItem>();
+
+
+
+    private static final int COUNT = 25;
+
+    static {
+        addItem(new PlaceholderItem("GR1", "123", "456", "Grainger", "12/12/21"));
+        addItem(new PlaceholderItem("BUS1", "123", "756", "Business", "12/12/21"));
+        addItem(new PlaceholderItem("SOC1", "23", "456", "Soc", "12/12/21"));
+    }
+
+    private static void addItem(PlaceholderItem item) {
+        ITEMS.add(item);
+//        ITEM_MAP.put(item.id, item);
+    }
+
+//    private static PlaceholderItem createPlaceholderItem(int position) {
+//        return new PlaceholderItem(String.valueOf(position), "Item " + position, makeDetails(position));
+//    }
+
+    private static String makeDetails(int position) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Details about Item: ").append(position);
+        for (int i = 0; i < position; i++) {
+            builder.append("\nMore details information here.");
+        }
+        return builder.toString();
+    }
+
+    /**
+     * A placeholder item representing a piece of content.
+     */
+    public static class PlaceholderItem {
+        public final String roomid;
+        public final String starttime;
+        public final String endtime;
+        public final String name;
+        public final String date;
+
+        public PlaceholderItem(String roomid, String starttime, String endtime, String name, String date) {
+            this.roomid = roomid;
+            this.starttime = starttime;
+            this.endtime = endtime;
+            this.name = name;
+            this.date = date;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+}
